8d77d3cbcc692646203b645c58b3dec88c994391
delannoykurt <delannoykurt666@gmail.com>
2022-10-23 21:36:22 +0200

    add reversing challenge
    add the next challenge, reversing array challenge
    
    add reversing challenge

--- 08-reversing-array.js
+++ 08-reversing-array.js
@@ -1,17 +1,20 @@
-# Reversing an array
+"use strict"
 
-Arrays have a reverse method that changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument by reversing its elements. Neither may use the standard reverse method.
+function reverseArray(T){
+	let tmp = [];
+	tmp = T.reverse();
+	return tmp;
+}
 
-Which variant do you expect to be useful in more situations? Which one runs faster?
-
-
-```js
 console.log(reverseArray(["A", "B", "C"]));
-// → ["C", "B", "A"];
+
+// ----------------------
 
 let arrayValue = [1, 2, 3, 4, 5];
-reverseArrayInPlace(arrayValue);
 
+function reverseArrayInPlace(arrayValue) {
+	arrayValue = reverseArray(arrayValue);
+}
+
+reverseArrayInPlace(arrayValue);
 console.log(arrayValue);
-// → [5, 4, 3, 2, 1]
-```
