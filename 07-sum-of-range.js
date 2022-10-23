8a6d2c653a3f7e6996981c093ce1f43b64622c46
delannoykurt <delannoykurt666@gmail.com>
2022-10-23 21:24:05 +0200

    add bean-counting challenge
    add the next challenge
    
    add challenge

--- 07-sum-of-range.js
+++ 07-sum-of-range.js
@@ -1,20 +1,35 @@
-# The sum of a range
+"use strict"
+
+function getIntervalNumber(start, stop){
+	let interval = [];
+	for(let i = start; i < stop; i++){
+		interval[i] = i;
+	}
+	return interval;
+}
 
 
-Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
+console.log(getIntervalNumber(1, 10));
 
-Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
+// --------------------------
 
-As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+function getIntervalNumberRestrict(start, stop, restrict){
+	let interval = [];
+	interval = getIntervalNumber(start, stop);
+	interval[interval.length + restrict].remove();
+	return interval;
+}
 
 
-```
-console.log(range(1, 10));
-// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(getIntervalNumberRestrict(5, 2, -1));
 
-console.log(range(5, 2, -1));
-// → [5, 4, 3, 2]
+// ----------------------------------
 
-console.log(sum(range(1, 10)));
-// → 55
-```
+let sum = 0;
+
+function sumOfRange(start, stop){
+	let tmp += getIntervalNumber(start, stop);
+	return tmp;
+}
+
+console.log(sumOfRange(getIntervalNumber(1, 10)));
