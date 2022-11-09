2fd6fe0ffb047264092a5419f553b72840385793
delannoykurt <delannoykurt666@gmail.com>
2022-11-09 20:55:41 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,36 +1,3 @@
-let distanceToPump = 50;
-let fuelLeft = 2;
-let mpg = 25;
-
-
-const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
-
-  let res = (distanceToPump - (fuelLeft * mpg));
-  if( res <= 0){
-    return true;
-  } else {
-    return false;
-  }
-  
-};
-
-zeroFuel(distanceToPump, fuelLeft, mpg);
-
-
-/*
-
-variables
-----------
-miles gallons fuel => entier
-
-
-vous faite du camping avec vos amis, lorsque vous revenez chez vous, vous realiser
-que la pompe la plus proche est a 5o gallons
-Vous n'avais pas realiser votre moyenne prevue de 25 miles par gallon
-En prenant compte de ces facteurs, ... il vous reste 2 gallons.
-ecrire une fonction dit vous dit si oui ou non il vous possible de se dirtiger vers la pompe ou non.
-
-
-si c'est possible retourner vrai, sinon non
-
-*/ 
+function getAge(inputString){
+// return the girl's correct age as an integer. Happy coding :) 
+}
