f3fbe037467f428fa5c1e3fa51b4cd9f4397efb5
delannoykurt <delannoykurt666@gmail.com>
2022-11-10 05:02:39 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,19 +1,5 @@
-function bmi(weight, height) {
-  let r = weight/height/height;
+function century(year) {
+  // Finish this :)
+  return Math.ceil(year/100);
   
-  if(r <= 18.5){
-     return "Underweight";
-  }
-  
-  if(r <= 25.0){
-     return "Normal";
-  }
-  
-  if(r <= 30.0){
-     return "Overweight";
-  }
-  
-  if(r > 30){
-     return "Obese";
-  }
-  
+}
