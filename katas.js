1f7f17e8153d97ff0b55f1b14f429e63e5b018e6
delannoykurt <delannoykurt666@gmail.com>
2022-11-10 09:04:26 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -5,5 +5,18 @@ function DNAStrand(dna){
       case "ATTGC": return "TAACG"; break;
       case "GTAT": return "CATA"; break;
   }
+  
+  if(dna.charAt() == "A"){
+    dna[dna.charAt()] = "T";
+  }
+   if(dna.charAt() == "T"){
+    dna[dna.charAt()] = "A";
+  }
+  if(dna.charAt() == "G"){
+    dna[dna.charAt()] = "C";
+  }
+   if(dna.charAt() == "C"){
+    dna[dna.charAt()] = "G";
+  }
 }
 
