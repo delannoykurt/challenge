6d05da2a54ea2a812954a9a94e4d39d85a330ae3
delannoykurt <delannoykurt666@gmail.com>
2022-11-10 03:33:48 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,3 +1,5 @@
-function getAge(inputString){
-// return the girl's correct age as an integer. Happy coding :) 
+function square(n){
+  return n*n;
 }
+
+//Now you have to write a function that takes an argument and returns the square of it.
