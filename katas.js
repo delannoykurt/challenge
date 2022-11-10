a7c5ef8f0cfe408f65db84c1b835cb62c6c897ef
delannoykurt <delannoykurt666@gmail.com>
2022-11-10 11:37:06 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,14 +1,6 @@
-function findAverage(array) {
-  // your code here
-  if(array == null && array == NaN && array == undefined && array == "" && array == [])
+function makeNegative(num) {
+  if(num == 0){
     return 0;
-  
-  if(array.length == 0)
-    return 0;
-  
-  let r = 0;
-  for(let n in array){
-    r += array[n];
   }
- return r / array.length;
+  return -num;
 }
