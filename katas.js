1c4ee61f86f559bb2b3a67cc9f52c4f90f4f3a0d
delannoykurt <delannoykurt666@gmail.com>
2022-11-10 03:35:04 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,5 +1,4 @@
-function square(n){
-  return n*n;
+function numberToString(num) {
+  // Return a string of the number here!
+  return num.toString();
 }
-
-//Now you have to write a function that takes an argument and returns the square of it.
