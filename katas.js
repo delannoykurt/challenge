9ee3f108ce174bb013b9ce81118d8aec5358ca38
delannoykurt <delannoykurt666@gmail.com>
2022-11-10 13:34:09 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,4 +1,17 @@
-function booleanToString(b){
-  //your code here
-  return b.toString();
+function basicOp(operation, value1, value2)
+{
+  if(operation == '+'){
+    return value1 + value2;
+  }
+  if(operation == '-'){
+    return value1 - value2;
+  }
+  if(operation == '*'){
+    return value1 * value2;
+  }
+  if(operation == '/'){
+    if(value1 == 0)
+      return 0;
+    return value1 / value2;
+  }
 }
