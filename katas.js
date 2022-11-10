5494831235793afb134006c108f026ab32c49e99
delannoykurt <delannoykurt666@gmail.com>
2022-11-10 14:31:17 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,3 +1,6 @@
-function boolToWord( bool ){
-  return bool == true ? 'Yes' : 'No';
+function setAlarm(employed, vacation){
+  if(employed == true && vacation == true) return false;
+  if(employed == false && vacation == true) return false;
+  if(employed == false && vacation == false) return false;
+  if(employed == true && vacation == false) return true;
 }
