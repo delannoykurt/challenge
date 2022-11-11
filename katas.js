612cb7e268879476250af1e3c73d8f91ad3fa810
delannoykurt <delannoykurt666@gmail.com>
2022-11-11 22:53:13 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,3 +1,9 @@
-function makeUpperCase(str) {
-  return str.toUpperCase();
+function arithmetic(a, b, operator){
+  if(operator == "add") return a + b;
+  if(operator == "subtract") return a - b;
+  if(operator == "multiply") return a * b;
+  if(operator == "divide") {
+    if(a == 0) return 1;
+    return Math.round(a/b);
+  }
 }
