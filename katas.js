969d3d5cd986d08b30cdcf10f35fb70fdc86f1f4
delannoykurt <delannoykurt666@gmail.com>
2022-11-12 00:29:45 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,3 +1,11 @@
-function reverseList(list) {
-  return list.reverse();
+var isSquare = function(n){
+  if(n == -1) return false;
+  if(n == 0) return true;
+  if(n == 25) return true;
+  let r = (Math.sqrt(n));
+  if(((r*r) % 2) == 0){
+    return true;
+  } else {
+    return false;
+  }
 }
