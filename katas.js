6006b6b3e840b7a5dfacb960f8b86fb9f1574eec
delannoykurt <delannoykurt666@gmail.com>
2022-11-11 22:42:01 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,8 +1,3 @@
-const areaOrPerimeter = function(l , w) {
-  if(l == 3 && w == 3){
-    return 9;
-  } 
-  if(l == 6 && w == 10){
-    return 32;
-  }
-};
+function sayHello(name) {
+  return 'Hello, ' + name;
+}
