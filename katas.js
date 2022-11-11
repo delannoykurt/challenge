77962122411eb52b830e41e70cdb7f40f5af4f58
delannoykurt <delannoykurt666@gmail.com>
2022-11-12 00:19:44 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,6 +1,3 @@
-const quarterOf = (month) => {
-  if(month <= 3) return 1;
-  if(month <= 6) return 2;
-  if(month <= 9) return 3;
-  if(month <= 12) return 4;
+function reverseList(list) {
+  return list.reverse();
 }
