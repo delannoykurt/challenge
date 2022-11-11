1c2c5f3122c6a199ec1ddf8061ed65458ba97659
delannoykurt <delannoykurt666@gmail.com>
2022-11-12 00:59:15 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,5 +1,11 @@
-class Kata {
-  static getVolumeOfCuboid(length, width, height) {
-    return length * width * height;
-  }
+function howMuchILoveYou(nbPetals) {
+    switch(nbPetals){
+        case 1: return "I love you"; break;
+        case 2: return "a little"; break;
+        case 3: return "a lot"; break;
+        case 4: return "passionately"; break;
+        case 5: return "madly"; break;
+        case 6: return "not at all"; break;
+        case 7: return "I love you"; break;
+    }
 }
