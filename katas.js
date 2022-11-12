36d9d7977e0cde193f39d6a1606922029b8b35a2
delannoykurt <delannoykurt666@gmail.com>
2022-11-12 16:13:06 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,6 +1,11 @@
-function lovefunc(flower1, flower2){
-  if((flower1%2) == 0 && (flower2%2) == 0){
-    return false;
+function findNeedle(haystack) {
+  let p = 0;
+  if(haystack.includes("needle")){
+    for(let i = 0; i < haystack.length; i++){
+      if(haystack[i] == "needle"){
+        p = i;
+      }
+    }
+    return `found the needle at position ${p}`;
   }
-  return true;
 }
