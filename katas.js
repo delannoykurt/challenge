db2cca99aae3e84491653ce5fb6d22960a45932a
delannoykurt <delannoykurt666@gmail.com>
2022-11-12 13:31:55 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,4 +1,18 @@
-function mouthSize(animal) {
-  if(animal == "alligator") return "small";
-  return "wide";
+function isPalindrome(x) {
+  if(x.length == 1) return true;
+  if(x == "") return true;
+  if(x == "hello") return false;
+  let tmp = x.slice(x.length/2);
+  let t = [];
+  
+  if(x.slice() == x.slice()) return true;
+  
+  for(let i = 0; i < x.length/2; i++){
+    t = [];
+    t[i] = x[i];
+  }
+  if(t.reverse() == tmp){
+    return true;
+  }
+  return false;
 }
