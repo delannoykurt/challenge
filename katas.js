7087db066f86e0abdfd98d88666a300940304d82
delannoykurt <delannoykurt666@gmail.com>
2022-11-13 00:27:31 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,7 +1,12 @@
-class SmallestIntegerFinder {
-  findSmallestInt(args) {
-    let t = [];
-    t = args.sort((a, b) => a - b);
-    return t[0];
+function strCount(str, letter){  
+  let l = 0;
+  if(str == ""){
+    return 0;
   }
+  for(let i = 0; i < str.length; i++){
+    if(str[i] == letter){
+      l++;
+    }
+  }
+  return l;
 }
