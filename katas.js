3d7a345a1c03a4d7d8de97cd4808d8e9e6e8ee4a
delannoykurt <delannoykurt666@gmail.com>
2022-11-12 01:06:26 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,11 +1,7 @@
-function howMuchILoveYou(nbPetals) {
-    switch(nbPetals){
-        case 1: return "I love you"; break;
-        case 2: return "a little"; break;
-        case 3: return "a lot"; break;
-        case 4: return "passionately"; break;
-        case 5: return "madly"; break;
-        case 6: return "not at all"; break;
-        case 7: return "I love you"; break;
-    }
+function between(a, b) {
+  let t = [];
+  for(let i = a; i <= b; i++){
+    t.push(i);
+  }
+  return t;
 }
