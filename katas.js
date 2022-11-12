482b6537678126e053026a72d49b259bc467c7f8
delannoykurt <delannoykurt666@gmail.com>
2022-11-12 13:37:54 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,18 +1,9 @@
-function isPalindrome(x) {
-  if(x.length == 1) return true;
-  if(x == "") return true;
-  if(x == "hello") return false;
-  let tmp = x.slice(x.length/2);
-  let t = [];
-  
-  if(x.slice() == x.slice()) return true;
-  
-  for(let i = 0; i < x.length/2; i++){
-    t = [];
-    t[i] = x[i];
+var number=function(array){
+  if(array == []) return array;
+  let i = 0;
+  for(let n in array){
+    array[n] = String(i+1) + ": " + array[i];
+    i++;
   }
-  if(t.reverse() == tmp){
-    return true;
-  }
-  return false;
+  return array;
 }
