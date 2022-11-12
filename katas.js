6eaac86ea3d752ae3c54b945a1adb8760485a488
delannoykurt <delannoykurt666@gmail.com>
2022-11-12 18:34:42 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,6 +1,6 @@
-function invert(array) {
-  for(let i = 0 ; i < array.length; i++){
-    array[i] = -array[i];
+function feast(beast, dish) {
+  if(beast[0] == dish[0] && beast[beast.length-1] == dish[dish.length-1]){
+    return true;
   }
-  return array;
+  return false;
 }
