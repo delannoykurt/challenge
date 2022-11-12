3d0fb09b71376a9e34c06de110d0d2963248333a
delannoykurt <delannoykurt666@gmail.com>
2022-11-12 11:11:40 +0100

    add katas
    resolv problem with javascript langage from codeware plateforme

--- katas.js
+++ katas.js
@@ -1,3 +1,9 @@
-const stringToNumber = function(str){
-  return Number(str);
+function areYouPlayingBanjo(name) {
+  for(let i = 0; i < name.length; i++){
+    if(name[i] == "R" || name[i] == "r"){
+      return name + " plays banjo";
+    } else {
+      return name + " does not play banjo";
+    }
+  }
 }
