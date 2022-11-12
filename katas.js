d7b0233478de611eb49e01da962c7b2539cd9f15
delannoykurt <delannoykurt666@gmail.com>
2022-11-12 18:39:09 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,6 +1,11 @@
-function feast(beast, dish) {
-  if(beast[0] == dish[0] && beast[beast.length-1] == dish[dish.length-1]){
-    return true;
+function finalGrade (exam, projects) {
+  if(exam > 90 && projects > 10) {
+    return 100;
   }
-  return false;
+  if(exam > 75 && projects >= 5){
+    return 90;
+  }
+  if(exam > 50 && projects >= 2){
+    return 75;
+  }
 }
