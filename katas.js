e8495e1bc8dfb83bb9a12c87c064b60b357a0b0d
delannoykurt <delannoykurt666@gmail.com>
2022-11-14 10:10:06 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,3 +1,14 @@
-function howManyLightsabersDoYouOwn(name) {
-  return (name == "Zach") ? 18 : 0;
-}
+function switchItUp(number){
+  switch(number){
+      case 0: return "Zero"; break;
+      case 1: return "One"; break;
+      case 2: return "Two"; break;
+      case 3: return "Three"; break;
+      case 4: return "Four"; break;
+      case 5: return "Five"; break;
+      case 6: return "Six"; break;
+      case 7: return "Seven"; break;
+      case 8: return "Height"; break;
+      case 9: return "Nine"; break;
+      
+  }
