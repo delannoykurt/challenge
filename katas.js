92d4e1331ca164aa311ad7e5be3a4c31238ca390
delannoykurt <delannoykurt666@gmail.com>
2022-11-14 15:32:50 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,3 +1,8 @@
-function move (position, roll) {
-  return roll * 2 + position;
+function index(array, n){
+  let indice = array[n];
+  
+  if(n > array.length) return -1;
+  
+  let r = indice**n;
+  return r;
 }
