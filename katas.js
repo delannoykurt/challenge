7b0d57e51eac2a6a475d635370752864a7f041ed
delannoykurt <delannoykurt666@gmail.com>
2022-11-15 14:12:39 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,3 +1,9 @@
-function problem(x){
-  return (typeof x == 'string' || x === "") ? "Error" : x*50+6;
+function removeEveryOther(arr){
+  let s = arr.length;
+  for(let i = 0; i < s; i++){
+    arr.splice(i+1,1);
+  }
+  return arr;
 }
+// [0,1,2]
+// [0,1,2,3,4,5,...]
