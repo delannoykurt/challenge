e64192ba87a40759f270d5031459805c1dabd4a1
delannoykurt <delannoykurt666@gmail.com>
2022-11-15 14:24:49 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,3 +1,5 @@
-function solution(str){
-  return str.split('').reverse().join('');
+function nameShuffler(str){
+  return str.split(' ').reverse().join(' ');
+  //let a = str.split('').slice(0,1);
+  
 }
