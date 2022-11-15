3de15b0b61ad658e5a9748a3b5dd8b452da14068
delannoykurt <delannoykurt666@gmail.com>
2022-11-15 09:29:36 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,3 +1,11 @@
-function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
-  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+function litres(time) {
+  if(time == 0 || time < 2) {
+    return 0;
+  }
+  if(time == 2){
+    return 1;
+  }
+  if(time > 3){
+    return Math.floor(time*0.5);
+  }
 }
