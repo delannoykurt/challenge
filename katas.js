6226f8d347ffca81cb8e3d8aae4968514078ee70
delannoykurt <delannoykurt666@gmail.com>
2022-11-16 21:39:23 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,3 +1,6 @@
-function combat(health, damage) {
-  return (damage > health) ? 0 : health - damage;
+function squareDigits(num){
+  let x = (num).toString().split('');
+  x = x.map(v => v**2);
+  x = x.join('');
+  return Number(x);
 }
