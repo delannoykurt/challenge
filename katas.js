76d61a9461abd642a5a1cdfeb7c8950f27a1e569
delannoykurt <delannoykurt666@gmail.com>
2022-11-16 22:00:14 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,6 +1,15 @@
-function squareDigits(num){
-  let x = (num).toString().split('');
-  x = x.map(v => v**2);
-  x = x.join('');
-  return Number(x);
+const getMsg = (n) => `Player ${n} won!`;
+
+function rps(a,b){
+  if(a == 'rock' && b == 'scissors') return getMsg(1);
+  if(a == 'scissors' && b == 'paper') return getMsg(1);
+  if(a == 'paper' && b == 'rock') return getMsg(1);
+  
+  if(a == 'scissors' && b == 'rock') return getMsg(2);
+  if(a == 'paper' && b == 'scissors') return getMsg(2);
+  if(a == 'rock' && b == 'paper') return getMsg(2);
+  
+  if(a == 'rock' && b == 'rock') return 'Draw!';
+  if(a == 'scissors' && b == 'scissors') return 'Draw!';
+  if(a == 'paper' && b == 'paper') return 'Draw!';
 }
