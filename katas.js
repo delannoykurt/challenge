f610f04cb486888f96cbd6218b10641285f089cd
delannoykurt <delannoykurt666@gmail.com>
2022-11-16 17:34:06 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,3 +1,21 @@
-function xor(a, b) {
-  return (a == b) ? false : true;
+function solution(nums){
+  if(nums == null || nums == []){
+    return [];
+  } 
+  
+  let tmp = [];
+  let n = 0;
+  
+  while(n < nums.length){
+    for(let i = 0; i < nums.length; i++){
+      if(nums[n] < nums[i]){
+        tmp = nums[n];
+        nums[n] = nums[i];
+        nums[i] = tmp;
+      }
+    }
+    n++;
+  }
+  
+  return nums;
 }
