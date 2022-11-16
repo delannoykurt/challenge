cfe127a7ee994662c53e080befa51476082c268b
delannoykurt <delannoykurt666@gmail.com>
2022-11-16 17:52:34 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,21 +1,16 @@
-function solution(nums){
-  if(nums == null || nums == []){
-    return [];
-  } 
-  
-  let tmp = [];
-  let n = 0;
-  
-  while(n < nums.length){
-    for(let i = 0; i < nums.length; i++){
-      if(nums[n] < nums[i]){
-        tmp = nums[n];
-        nums[n] = nums[i];
-        nums[i] = tmp;
-      }
-    }
-    n++;
+var humanYearsCatYearsDogYears = function(humanYears) {
+  let a = 15;
+  let b = 9;
+  let c = 4;
+  let d = 5;
+  let cat = a;
+  let dog = a;
+  if(humanYears == 1){
+    return [humanYears, cat, dog];
+  } else if(humanYears == 2){
+    return [humanYears, cat+b, dog+b];
+  } else if(humanYears >= 3){
+    return [humanYears, cat+b+((humanYears-2) *c), dog+b+((humanYears-2)*d)];
   }
   
-  return nums;
 }
