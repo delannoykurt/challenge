9457d53ac5ac5a1de59d5fe155a5874b4b1fb41d
delannoykurt <delannoykurt666@gmail.com>
2022-11-16 14:50:56 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,10 +1,7 @@
-function removeExclamationMarks(s) {
-  let t = [];
-  t = s.split('');
-  for(let i in t){
-    if(t[i] == '!'){
-      t[i] = '';
-    }
+function grow(x){
+  let tmp = x[0];
+  for(let i = 0; i < x.length-1; i++){
+    tmp = tmp * x[i+1];
   }
-  return t.join('');
+  return tmp;
 }
