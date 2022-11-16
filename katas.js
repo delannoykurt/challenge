9cb1ca0a081679386cb96f5b5aa8d88bc07d4941
delannoykurt <delannoykurt666@gmail.com>
2022-11-16 11:03:05 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,3 +1,16 @@
-function stringToArray(string){
- return string.split(' ');
+function toCsvText(array) {
+  let res = [];
+  for(let num of array){
+    res.push(num);
+  }
+  res = res.join(' ');  // '1,2,3,4,5...'
+  
+  for(let i in res){
+    if(res[i] == ' '){
+      res[i] = '\'\n+\'';
+    }
+  }
+  
+  return res.split(' ').join('\n');
+  
 }
