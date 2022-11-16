3a5f57359612d8972992ea7996e2ad1f735abfc1
delannoykurt <delannoykurt666@gmail.com>
2022-11-16 21:18:15 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,11 +1,3 @@
-function peopleWithAgeDrink(old) {
-  if(old < 14){
-    return 'drink toddy';
-  }else if(old < 18){
-     return 'drink coke';
-  }else if(old < 21){
-     return 'drink beer';
-  }else if(old >= 21){
-     return 'drink whisky';
-  }
-};
+function hexToDec(hexString){
+  return parseInt(hexString, 16);
+}
