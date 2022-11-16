85216731f565d4c3bbfb1db44edad76c7710eedd
delannoykurt <delannoykurt666@gmail.com>
2022-11-16 16:16:41 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,16 +1,3 @@
-function filter_list(l) {
-  for(let i =0; i < l.length; i++){
-    if(typeof l[i] != 'number'){
-      l[i] = -1;
-    }
-  }
-  let t = [];
-  for(let i = 0; i < l.length; i++){
-    if(l[i] >= 0){
-      t.push(l[i]);
-    }
-  }
-  l = t;
-  
-  return l;
+function xor(a, b) {
+  return (a == b) ? false : true;
 }
