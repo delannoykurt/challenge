f528f1882e8875fddbc0252c4cc145c90bf24326
delannoykurt <delannoykurt666@gmail.com>
2022-11-17 15:57:28 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,3 +1,19 @@
-function howManyLightsabersDoYouOwn(name) {
-  return (name === "Zach") ? 18 : 0;
+function returnSmall(numbers) {
+  let v = numbers.indexOf(Math.min(...numbers));
+  return v;
+}
+
+function removeSmallest(numbers){
+  // debug
+  // -------------------
+  
+  console.log(numbers);
+  
+  // -------------------
+  
+  let minimum = returnSmall(numbers);
+  let tmp = numbers.slice();
+  tmp.splice(minimum,1);
+  return tmp;
+  
 }
