76e473154d012fd23dde18e21b889831a32fa5de
delannoykurt <delannoykurt666@gmail.com>
2022-11-21 08:03:07 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,11 +1,30 @@
-function makeNegative(num) {
-  if(num == 0){
-    return 0;
+function rentalCarCost(d) {
+  // 40$ / j
+  // += 7j / remise de 50$
+  // += 3j / remise de 20$
+  let price = 40;
+  let total = 0;
+  let reduce = 0;
+  let n =0;
+  
+  if(d < 3){
+    reduce = 0;
+    while(n < d){
+      total += price - reduce;
+      n++;
+    }
+  } else if(d >= 3 && d < 7){
+    reduce = 20;
+    while(n < d){
+      total += price;
+      n++;
+    }
+  } else if(d >= 7){
+    reduce = 50;
+    while(n < d){
+      total += price;
+      n++;
+    }
   }
-  if(num < 0){
-    return num;
-  }
-  if(num > 0){
-    return num * -1;
-  }
+  return total - reduce;
 }
