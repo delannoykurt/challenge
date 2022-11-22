02a359372282a68fb67d2c3452eb65d0ca3034ae
delannoykurt <delannoykurt666@gmail.com>
2022-11-22 07:21:15 +0100

    add katas
    resolv the new challenge
    
    resolv problems

--- katas.js
+++ katas.js
@@ -1,30 +1,13 @@
-function rentalCarCost(d) {
-  // 40$ / j
-  // += 7j / remise de 50$
-  // += 3j / remise de 20$
-  let price = 40;
-  let total = 0;
-  let reduce = 0;
-  let n =0;
-  
-  if(d < 3){
-    reduce = 0;
-    while(n < d){
-      total += price - reduce;
-      n++;
-    }
-  } else if(d >= 3 && d < 7){
-    reduce = 20;
-    while(n < d){
-      total += price;
-      n++;
-    }
-  } else if(d >= 7){
-    reduce = 50;
-    while(n < d){
-      total += price;
-      n++;
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  let age = sonYearsOld - dadYearsOld;
+  if(age == 0){
+    return 0;
+  } else {
+    let a =  sonYearsOld*2-dadYearsOld;
+    if(a < 0){
+      return a*-1;
+    } else {
+      return a;
     }
   }
-  return total - reduce;
 }
