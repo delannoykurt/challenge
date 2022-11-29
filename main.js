683bd2be91464c0cbbe84f8a66a72580f7cc6bb7
delannoykurt <delannoykurt666@gmail.com>
2022-11-29 23:14:08 +0100

    add element has resolved ...

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 "use strict"
 
-
-// L'idee : serais de commencer a eliminer 0.3.6..., une personne sur 3
-// ainsi il y aurais un ecart de 3, donc sur un total de 51 personnes
-// la derniere serait epargner.
-function josephus() {
-	let nPrisoners = [];
-	for(let i = 0; i < 41; i++){
-		nPrisoners[i] = ((i % 2) == 0) ? 1 : 0;
-	}
-	
-	return nPrisoners;
+String.prototype.isUpperCase = function(args) {
+  let s = 0;
+  let tmp = args.split('');
+  tmp.forEach((v) => {
+    if(v.toUpperCase()){
+      s++;
+    } else {
+      s--;
+    }
+  })
+  if(s == args.length){
+    return true;
+  } else {
+    return false;
+  }
 }
-
-// -------------------
-console.log(josephus());
-
-// to be continued ...
